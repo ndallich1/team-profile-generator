2f18576ad9055240e35ac0600e727d9906957713
ndallich1 <ndallich@gmail.com>
2023-02-20 19:46:16 -0500

    added html using bulma framework

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
 const fs = require("fs");
 const prompts = require("./lib/prompts");
-const engineer = require("./lib/Engineer");
-const intern = require("./lib/Intern");
-const manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const team = [];
 
 // Initialize a new employee object
-const employee = new Employee();
+// const init = () => {
+//   promptUser() // update this function
+//     .then((answers) => writeFile("index.html", generateHTML(answers)))
+//     .then(() => console.log("Successfully created index.html!"))
+//     .catch((err) => console.error(err));
+// };
 
-employee.create();
+const managerPrompts = () => {
+  inquirer.prompt(prompts.managerPrompts).then((answers) => {
+    const { name, id, email, officeNumber } = answers; // object destructuring
+    const manager = new Manager(name, id, email, officeNumber);
+    team.push(manager);
+    nextQuestion();
+  });
+};
+
+managerPrompts();
+
+const nextQuestion = () => {
+  inquirer.prompt(prompts.menu).then((answers) => {
+    if (answers.employeeType == "Engineer") engineerPrompts();
+    if (answers.employeeType == "Intern") internPrompts();
+    if (answers.employeeType == "Finish building team") generateHTML();
+  });
+};
+
+const engineerPrompts = () => {
+  inquirer.prompt(prompts.engineerPrompts).then((answers) => {
+    const { name, id, email, github } = answers; // object destructuring
+    const engineer = new Engineer(name, id, email, github);
+    team.push(engineer);
+    nextQuestion();
+  });
+};
+
+const internPrompts = () => {
+  inquirer.prompt(prompts.internPrompts).then((answers) => {
+    const { name, id, email, school } = answers; // object destructuring
+    const intern = new Intern(name, id, email, school);
+    team.push(intern);
+    nextQuestion();
+  });
+};
+
+const generateHTML = () => {
+  console.log(team);
+};
